0950cb10bf391cdb0d7da8dd4c83d7a14daee652
najish farooqui <najish.farooqui@gmail.com>
2018-12-21 22:33:07 +0530

    search bar in cpmpany page is implemented not tested yet

---
+++ app/src/main/java/com/example/applligent/nagoriengineering/ServiceInfoActivity.java
@@ -0,0 +1,31 @@
+package com.example.applligent.nagoriengineering;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.applligent.nagoriengineering.databinding.ActivityServiceInfoBinding;
+
+public class ServiceInfoActivity extends AppCompatActivity {
+    public int partsId;
+    ItemDao itemDao;
+    ActivityServiceInfoBinding binding;
+    Item item;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_info);
+        itemDao = MyNagoriApplication.getDatabase().itemDao();
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_parts_info);
+        partsId = getIntent().getIntExtra("parts_info", -1);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        item = itemDao.get(partsId);
+        binding.setItem(item);
+    }
+}
